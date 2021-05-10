d0fc4e7cafcf0625b743fe9c1253535ea09ee4c3
Coby Leavitt <coby.leavitt@gmail.com>
2021-05-10 12:58:19 -0600

    added remainder of roster

--- client-side-roster/js/main.js
+++ client-side-roster/js/main.js
@@ -3,7 +3,7 @@ document.querySelector("button").addEventListener("click", getPlayerInfo)
 async function getPlayerInfo() {
     const playerName = document.querySelector("input").value
     try{
-        const res = await fetch(`http://localhost:1337/api/players/${playerName}`)
+        const res = await fetch(`https://jazz-api.herokuapp.com/api/players/${playerName}`)
         const data = await res.json()
         document.querySelector("h2").innerText = data.position
     } catch(err) {
--- server.js
+++ server.js
@@ -6,16 +6,106 @@ const PORT = 1337
 app.use(cors())
 
 let players = {
+    "udoka azubuike": {
+        "firstName": "Udoka",
+        "lastName": "Azubuike",
+        "number": 20,
+        "position": "Center-Forward"
+    },
+    "bojan bogdanovic": {
+        "firstName": "Bojan",
+        "lastName": "Bogdanovich",
+        "number": 44,
+        "position": "Forward"
+    },
+    "jarrell brantley": {
+        "firstName": "Jarrell",
+        "lastName": "Brantley",
+        "number": 5,
+        "position": "Forward"
+    },
     "jordan clarkson": {
         "firstName": "Jordan",
         "lastName": "Clarkson",
         "number": 00,
         "position": "Guard"
     },
+    "mike conley": {
+        "firstName": "Mike",
+        "lastName": "Conley",
+        "number": 10,
+        "position": "Guard"
+    },
+    "derrick favors": {
+        "firstName": "Derrick",
+        "lastName": "Favors",
+        "number": 15,
+        "position": "Forward"
+    },
+    "trent forrest": {
+        "firstName": "Trent",
+        "lastName": "Forrest",
+        "number": 3,
+        "position": "Guard"
+    },
+    "rudy gobert": {
+        "firstName": "Rudy",
+        "lastName": "Gobert",
+        "number": 27,
+        "position": "Center"
+    },
+    "elijah hughes": {
+        "firstName": "Elijah",
+        "lastName": "Hughes",
+        "number": 33,
+        "position": "Guard"
+    },
+    "ersan ilyasova": {
+        "firstName": "Ersan",
+        "lastName": "Ilyasova",
+        "number": 77,
+        "position": "Forward"
+    },
+    "joe ingles": {
+        "firstName": "Joe",
+        "lastName": "Ingles",
+        "number": 2,
+        "position": "Forward-Guard"
+    },
     "donovan mitchell": {
         "firstName": "Donovan",
         "lastName": "Mitchell",
         "number": 45,
+        "position": "Guard"
+    },
+    "juwan morgan": {
+        "firstName": "Juwan",
+        "lastName": "Morgan",
+        "number": 16,
+        "position": "Forward"
+    },
+    "georges niang": {
+        "firstName": "Georges",
+        "lastName": "Niang",
+        "number": 31,
+        "position": "Forward"
+    },
+    "royce o'neale": {
+        "firstName": "Royce",
+        "lastName": "O'Neale",
+        "number": 23,
+        "position": "Forward"
+    },
+    "miye oni": {
+        "firstName": "miye",
+        "lastName": "oni",
+        "number": 81,
+        "position": "Guard-Forward"
+    },
+    "matt thomas": {
+        "firstName": "Matt",
+        "lastName": "Thomas",
+        "number": 21,
         "position": "Guard"
     },
     "unknown": {
